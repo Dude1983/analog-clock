dd674d892eac6914b53e0299066e3542c58862d7
Carsten Pleiser <carsten@foundersandcreators.com>
2018-05-04 20:15:59 +0100

    Fixed quirky full minute behaviour

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+console.log(hours + ":" + minutes + ":" + seconds);
+
+let hrPosition = (hours * 360 / 12) + (minutes * (360 / 60) / 12);
+let minPosition = (minutes * 360 / 60) + (seconds * (360 / 60) / 60);
+let secPosition = seconds * 360 /  60;
+
 
 setInterval(function runClock() {
-  var date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  console.log(hours + ":" + minutes + ":" + seconds);
-
-  let hrPosition = (hours * 360 / 12) + (minutes * (360 / 60) / 12);
-  let minPosition = (minutes * 360 / 60) + (seconds * (360 / 60) / 60);
-  let secPosition = seconds * 360 /  60;
-  console.log(hrPosition);
+  hrPosition += (3/360);
+  minPosition += (6/60);
+  secPosition += 6;
 
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-}, 1000)(); 
+}, 1000)();
